9b0516a6667a9cf3622c030cfe79dfaa4b37a1cf
Brian Dudey <dudey.brian@gmail.com>
2018-02-24 14:33:50 -0600

    Thunderball

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const app = express();
 //app(express App to register this route handler with), get(watch for incoming http requests with this SPECIFIC (get, post, put, delete) method),
 //"/" (is the route portion of the handler), req (request), res(response), res.send({hi: 'there'})(json object that we are responding with)
 app.get('/', (rec, res) => {
-  res.send({'aya says': 'hello'})
+  res.send({'aya says': 'I said, good day, sir'});
 });
 
 
 //dynamically decide what port we are listening to (i.e. look at the underlying environment to determine the port - heroku determines this || we use 5000 (<-development))
-const PORT = process.env.PORT ||5000 
+const PORT = process.env.PORT ||5000;
 
 //express is telling node to look for any traffic coming from this route.
 app.listen(5000);
